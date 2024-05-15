3ef0bcb19f56d737e85d1782766cf940fd212b1d
Uxdruh <uxdruh@gmail.com>
2024-05-15 22:30:38 +0800

    Create 3 components, make add button interactive, change css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList todos={todos} handleAddTodos={handleAddTodos}/>
     </>
   )
 }
---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+  const { children } = props
+  return (
+    <li className='todoItem'>
+      { children }
+      <div className='actionContainer'>
+        <i className="fa-solid fa-pen-to-square"></i>
+        <i className="fa-regular fa-trash-can"></i>
+      </div>
+      
+    </li>
+  )
+}
